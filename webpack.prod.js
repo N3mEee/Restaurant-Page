3c7a19f4e8d2f2ba18d3533acd65015cde88bf00
N3mEee <n3mebusiness@gmail.com>
2022-12-12 17:47:46 +0100

    Add new font

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const htmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-    mode: 'production',
+    mode: "production",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
@@ -16,6 +16,10 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
                 type: "asset/resource",
             },
         ],
