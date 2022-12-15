072afb87c9d1b1e4eb56fa8ddda64446912bc455
N3mEee <n3mebusiness@gmail.com>
2022-12-15 15:25:59 +0100

    Add content and style for #about-page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,41 +1,53 @@
-import Icon from "../assets/about.png";
 import "../styles/about.css";
 
 export default function aboutPage() {
     const $content = document.querySelector("#content");
+    $content.classList = "about-page";
 
-    const $info = document.createElement("div");
-    $info.classList.add("info", "info-about");
+    //create container element
+    const $container = document.createElement("div");
+    $container.classList = "container";
+    $content.appendChild($container);
 
-    const $h1 = document.createElement("h1");
-    $h1.innerText = "Time to order food";
-    $info.appendChild($h1);
+    //create tittle
+    const $title = document.createElement("h1");
+    $title.textContent = "Restaurant Page: About Us";
+    $container.appendChild($title);
 
-    const $h2 = document.createElement("h2");
-    $h2.innerText = "Find our restaurant in your area";
-    $info.appendChild($h2);
+    //create content
+    const $p1 = document.createElement("p");
+    $p1.textContent =
+        "Since our modest beginnings in 2005 with a little space in ‘City Name’’s stylish Yorkville locale, ‘Organization Name’ ‘s development has been enlivened with the energy to cook and serve solid, Indian-roused takeout food.";
+    $container.appendChild($p1);
 
-    const $form = document.createElement("form");
-    $info.appendChild($form);
+    const $p2 = document.createElement("p");
+    $p2.textContent =
+        "In contrast to other Indian eateries, ‘Organization Name’ was made with the explicit expectation to appear as something else.";
+    $container.appendChild($p2);
 
-    const $label = document.createElement("label");
-    $label.innerText = "Your ABOUT Address";
-    $label.setAttribute("for", "address");
-    $form.appendChild($label);
+    const $p3 = document.createElement("p");
+    $p3.textContent =
+        "We realize numerous individuals love Indian sustenance, yet a large number of them loathe or are unconscious of the regularly unfortunate fixings that make run-of-the-mill Indian nourishment taste so great.";
+    $container.appendChild($p3);
 
-    const $input = document.createElement("input");
-    $input.setAttribute("type", "text");
-    $input.setAttribute("name", "address");
-    $input.setAttribute("id", "address");
-    $form.appendChild($input);
+    const $p4 = document.createElement("p");
+    $p4.textContent =
+        "Our menu highlights things that utilization the sound and fragrant flavors, however, forgets the stuffing ghee, spread, oil, and overwhelming cream.";
+    $container.appendChild($p4);
 
-    const $rightImg = document.createElement("div");
-    $rightImg.classList.add("right-img");
+    const $p5 = document.createElement("p");
+    $p5.textContent =
+        "‘Organization Name’ has developed to incorporate four superb takeout areas in ‘City Name’ with additional to come sooner rather than later. Our group takes pride in the way that we can furnish our new and faithful clients with extraordinary tasting Indian-roused nourishment that is not normal for that at some other Indian eatery you visit.";
+    $container.appendChild($p5);
 
-    const $img = new Image();
-    $img.src = Icon;
-    $rightImg.appendChild($img);
+    const $p6 = document.createElement("p");
+    $p6.textContent =
+        "We perceive that a few people are as yet searching for the run-of-the-mill Indian nourishment, and that is fine with us. Our disclaimer is that on the off chance that you’re anticipating overwhelming, slick, undesirable Indian nourishment, ‘Organization Name’ isn’t the place for you.";
+    $container.appendChild($p6);
 
-    $content.appendChild($info);
-    $content.appendChild($rightImg);
+    //create copyright
+    const $copyright = document.createElement("em");
+    $copyright.textContent =
+        "This template is from : https://thewordyboy.com/restaurant-about-us/";
+    $container.appendChild($copyright);
 }
--- src/pages/menu.js
+++ src/pages/menu.js
@@ -1,41 +1,6 @@
-import Icon from "../assets/menu.png";
 import "../styles/menu.css";
 
 export default function menuPage() {
     const $content = document.querySelector("#content");
-
-    const $info = document.createElement("div");
-    $info.classList.add("info", "info-menu");
-
-    const $h1 = document.createElement("h1");
-    $h1.innerText = "Time to order food";
-    $info.appendChild($h1);
-
-    const $h2 = document.createElement("h2");
-    $h2.innerText = "Find our restaurant in your area";
-    $info.appendChild($h2);
-
-    const $form = document.createElement("form");
-    $info.appendChild($form);
-
-    const $label = document.createElement("label");
-    $label.innerText = "Your MENU Address";
-    $label.setAttribute("for", "address");
-    $form.appendChild($label);
-
-    const $input = document.createElement("input");
-    $input.setAttribute("type", "text");
-    $input.setAttribute("name", "address");
-    $input.setAttribute("id", "address");
-    $form.appendChild($input);
-
-    const $rightImg = document.createElement("div");
-    $rightImg.classList.add("right-img");
-
-    const $img = new Image();
-    $img.src = Icon;
-    $rightImg.appendChild($img);
-
-    $content.appendChild($info);
-    $content.appendChild($rightImg);
+    $content.classList = "menu-page";
 }
